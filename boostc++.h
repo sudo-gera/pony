354654822f27974d6001bd64a2f2332483417ff9
sudo-gera <gera.tatarinov.999@gmail.com>
2022-06-23 01:53:10 +0300

    *interesting commit message*

--- boostc++.h
+++ boostc++.h
@@ -2,25 +2,25 @@
 #if __has_include("boost/numeric/ublas/vector.hpp")
 	#include <boost/numeric/ublas/vector.hpp>
 #else
-	#warning "boost/numeric/ublas/vector is not available"
+	// #warning "boost/numeric/ublas/vector is not available"
 	#undef BOOST_INCLUDED
 #endif
 #if __has_include("boost/numeric/ublas/matrix.hpp")
 	#include <boost/numeric/ublas/matrix.hpp>
 #else
-	#warning "boost/numeric/ublas/matrix is not available"
+	// #warning "boost/numeric/ublas/matrix is not available"
 	#undef BOOST_INCLUDED
 #endif
 #if __has_include("boost/numeric/ublas/io.hpp")
 	#include <boost/numeric/ublas/io.hpp>
 #else
-	#warning "boost/numeric/ublas/io is not available"
+	// #warning "boost/numeric/ublas/io is not available"
 	#undef BOOST_INCLUDED
 #endif
 #if __has_include("boost/pfr/core.hpp")
 	#include <boost/pfr/core.hpp>
 #else
-	#warning "boost/pfr/core is not available"
+	// #warning "boost/pfr/core is not available"
 	#undef BOOST_INCLUDED
 #endif
 
@@ -28,6 +28,6 @@
 	#include <boost/multiprecision/cpp_int.hpp>
 	using namespace boost::multiprecision;
 #else
-	#warning "boost/multiprecision/cpp_int is not available"
+	// #warning "boost/multiprecision/cpp_int is not available"
 	#undef BOOST_INCLUDED
 #endif
